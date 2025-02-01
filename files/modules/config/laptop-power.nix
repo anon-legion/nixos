df477d4b7c3aacd6ff0efb2fb00bb1c86bed576c
thecomeback <g.sync.mobile1@gmail.com>
2025-02-01 17:18:13 +0800

    fix laptop-power module syntax error

--- files/modules/config/laptop-power.nix
+++ files/modules/config/laptop-power.nix
@@ -4,6 +4,6 @@
     # Disable built-in Gnome power management daemon
     power-profiles-daemon.enable = false;
     # Enable TLP power management daemon
-    services.tlp.enable = true;
+    tlp.enable = true;
   };
 }
