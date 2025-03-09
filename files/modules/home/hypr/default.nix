fac660abb6fd82b7f5719cd9a5f47c4fcb194ba6
thecomeback <g.sync.mobile1@gmail.com>
2025-03-09 20:20:18 +0800

    Add rofi module to hyprland home-manager deps

--- files/modules/home/hypr/default.nix
+++ files/modules/home/hypr/default.nix
@@ -6,6 +6,7 @@
     ./misc/gtk
     ./misc/wallpaper
     ./misc/xdg
+    ./rofi
     ./swaync
     ./waybar
     ./wlogout
--- files/modules/home/hypr/rofi/default.nix
+++ files/modules/home/hypr/rofi/default.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  programs.rofi = {
-    enable = true;
-    package = pkgs.rofi-wayland;
-    plugins = with pkgs; [
-      (rofi-calc.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-    ];
+  xdg.configFile = {
+    "rofi/config.rasi" = {
+      source = ./config.rasi;
+    };
   };
 }
